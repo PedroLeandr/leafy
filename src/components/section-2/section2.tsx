22cebe016a8372abeff183428b0e9ae900418ab3
Ericksilva5 <ggd4rkuser5@gmail.com>
2025-05-26 22:16:59 +0100

    Correção de erros

--- src/components/section-2/section2.tsx
+++ src/components/section-2/section2.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 export default function Section2() {
   return (
     <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+      <h2 className="uppercase text-left font-bold text-3xl sm:text-4xl mb-8 text-[var(--color-text)]">
+        Parcerias
+      </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 items-center justify-center">
         {/* Pro Digital Key */}
         <div className="flex justify-center items-center">
